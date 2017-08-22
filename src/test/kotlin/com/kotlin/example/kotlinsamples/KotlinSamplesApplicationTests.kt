b58143a8bf9187b644a51c051e9368840177f885
Aleksandar Bosancic <aco.bosancic@gmail.com>
2017-08-22 10:04:47 +0200

    Init maven project

---
+++ src/test/kotlin/com/kotlin/example/kotlinsamples/KotlinSamplesApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kotlin.example.kotlinsamples
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinSamplesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
